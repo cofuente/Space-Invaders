766e5abb624444f5d6d02e1948b266486bcd7c62
Chiara Marcial Martínez <contact@cofuente.io>
2022-01-20 16:18:54 -0500

    disable rules
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- game.js
+++ game.js
@@ -1,13 +1,9 @@
+/* eslint-disable camelcase */
 import kaboom from 'kaboom'
 
-kaboom( {
+kaboom({
     scale: 1.5
 })
-
-add([
-    text('hello world'),
-    pos(120, 80),
-] )
 
 loadRoot('sprites/' )
 loadSprite('space', 'space.jpg')
@@ -27,4 +23,63 @@ loadRoot('sounds/')
 loadSound('rocket_thrust', 'rocket_thrust.wav')
 loadSound('laser', 'laser.wav')
 loadSound('explosion', 'explosion.mp3')
-loadSound('Steamtech-Mayhem_Looping','Steamtech-Mayhem_Looping.mp3')
+loadSound( 'Steamtech-Mayhem_Looping', 'Steamtech-Mayhem_Looping.mp3' )
+
+// first (and for now only) scene
+scene('main', () => {
+  layers([
+    'bg',
+    'obj',
+    'ui',
+  ], 'obj') // set obj layer to be the default layer
+
+  add([// background
+    sprite( 'space' ),
+    layer('bg')
+  ])
+
+  let score = 0
+
+  // ui
+  ui = add( [
+    layer('ui')
+  ])
+  ui.on('draw', () => { // called on every frame of the game
+    drawText({
+        text: 'Score: ' + score,
+        size: 14,
+        font: 'sink',
+        pos: vec2(8, 24)
+    })
+  })
+
+  const player = add([
+    sprite('ship'),
+    pos(160, 120), // TODO: set to center of screen
+    rotate(0),
+    origin('center'), // sets the sprite's origin to "center", so that when we rotate the ship, it will rotate around the middle of its sprite rather than the default top-left corner
+    solid(),
+    area(), // collision area for the sprite
+    'player',
+    'mobile',
+    'wraps',
+    {
+      turn_speed: 4.58,
+      speed: 0,
+      max_thrust: 48,
+      acceleration: 2,
+      deceleration: 4,
+      lives: 3,
+      can_shoot: true,
+      laser_cooldown: 0.5,
+      invulnerable: false,
+      invulnerablity_time: 3,
+      animation_frame: 0,
+      thrusting: false
+    }
+  ])
+
+})
+
+// initialize scene 'main'
+go( 'main' )
